37f256da16537647001fc683e1c5c9736ad67655
YOHANES VITO <yohanes.vito@ti.ukdw.ac.id>
2022-05-12 21:19:34 +0700

    add DetailHurufActivity

---
+++ AplikasiBelajarAnak/app/src/main/java/com/example/aba/ui/detailhuruf/DetailHurufActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aba.ui.detailhuruf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailHurufActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_huruf)
+    }
+}
---
+++ AplikasiBelajarAnak/app/src/main/java/com/example/aba/ui/huruf/HurufActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aba.ui.huruf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HurufActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_huruf)
+    }
+}
