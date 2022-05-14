4c0f007e01897f99498d989c9928cdb3d61465a1
YOHANES VITO <yohanes.vito@ti.ukdw.ac.id>
2022-05-14 14:10:36 +0700

    update huruf

--- AplikasiBelajarAnak/app/src/main/java/com/example/aba/ui/huruf/HurufActivity.kt
+++ AplikasiBelajarAnak/app/src/main/java/com/example/aba/ui/huruf/HurufActivity.kt
@@ -8,5 +8,6 @@ class HurufActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_huruf)
+        supportActionBar?.hide()
     }
 }
