2ca90ccf0249539b66251bef1d9ecb2b7ccfc463
rmscoal <rifky.m.s@mail.ugm.ac.id>
2022-05-14 21:12:55 +0700

    Fix Authorization & Firebase

--- backend/middleware/firebaseAdmin.js
+++ backend/middleware/firebaseAdmin.js
@@ -1,6 +1,6 @@
-/* Dapat UID dari token dan disimpan di req.users*/
+/* Dapat UID dari token dan disimpan di req.users */
 
-const admin = require('./config/firebaseAuth'); 
+const admin = require('./config/firebaseAuth'); // import admin from firebase initializeApp
 
 class Middleware {
     async decodeToken(req,res,next) {
@@ -8,57 +8,57 @@ class Middleware {
         const { authorization } = req.headers; 
 
         // check if the authorization headers are well configured
+        // this includes checking if headers.authorization exist
+        // then if the format in headers.authorization matches with the configured
         if (!authorization) return res.status(403).json({
             status: 'fail', 
             type: 'server/missing-authorization',
             message: 'Missing req.headers.authorization on request to the server. This is need for authorization!'
         })
 
-        if (!authorization.startWith('Bearer')) return res.status(400).json({
+        else if (!authorization.startWith('Bearer')) return res.status(400).json({
             status: 'fail', 
             type: 'server/missing-bearer',
             message: 'Missing Bearer in req.headers.authorization on request to the server. This is needed to extract the token!'
         })
 
-        if (authorization.split(' ').length !== 2) return res.status(400).json({
+        else if (authorization.split(' ').length !== 2) return res.status(400).json({
             status: 'fail',
             type: 'server/bearer-unrecognized',
             message: 'Bearer in req.headers.authorization is not well configured. This is need to extract the token!'
         })
-
         // after passing the authorization header checks, now checks the token
-        const token = req.headers.authorization.split(' ')[0]; // req.headers = {"Bearer $.token"} 
-        try {
-            // verifying uid
-            await admin.auth().verifyIdToken(token).then((decodedToken) => {
+        const token = authorization.split(' ')[1] // req.headers = {"Bearer $.token"} 
+        admin.auth().verifyIdToken(token)
+            .then((decodedToken) => {
                 const uid = decodedToken.uid;
-                req.user = uid;
-                next();
+                req.user = uid; // set uid in req.user
+                return next() // goes to the next middleware handler
             })
-        } catch (err) {
-            /*
-            on err for firebase tokens, such as sent was FMC token instead of id token or token has expired and many others!
-            err response: after executing console.log(err)
-            {
-                errorInfo: {
-                  code: 'auth/argument-error',
-                  message: 'Decoding Firebase ID token failed. Make sure you passed the entire string JWT which represents an ID token. See https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve an ID token.'
-                },
-                codePrefix: 'auth'
-            }
-            or
-            {
-                errorInfo: {
-                    code: 'auth/id-token-expired',
-                    message: 'Firebase ID token has expired. Get a fresh ID token from your client app and try again (auth/id-token-expired). See https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve an ID token.'
-                },
-                codePrefix: 'auth'
-            }
-            */
-            if (err.errorInfo.code === 'auth/internal-error') var statusCode = 500;
-            else var statusCode = 400; 
-            return res.status(statusCode).json({status: "fail", type: err.errorInfo.code, message: err.errorInfo.message}); // return with status codes
-        }
+            .catch((err) => {
+                /*
+                on err for firebase tokens, such as sent was FMC token instead of id token or token has expired and many others!
+                err response: after executing console.log(err)
+                {
+                    errorInfo: {
+                    code: 'auth/argument-error',
+                    message: 'Decoding Firebase ID token failed. Make sure you passed the entire string JWT which represents an ID token. See https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve an ID token.'
+                    },
+                    codePrefix: 'auth'
+                }
+                or
+                {
+                    errorInfo: {
+                        code: 'auth/id-token-expired',
+                        message: 'Firebase ID token has expired. Get a fresh ID token from your client app and try again (auth/id-token-expired). See https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve an ID token.'
+                    },
+                    codePrefix: 'auth'
+                }
+                */
+                if (err.errorInfo.code === 'auth/internal-error') var statusCode = 500;
+                else var statusCode = 400; 
+                return res.status(statusCode).json({status: "fail", type: err.errorInfo.code, message: err.errorInfo.message}); // return with status codes
+            })
     }
 }
 
