2ef19ab15b8d8ce13ab208c13b0fc2ca71490d56
VitoMAC <yohanes.vito@ti.ukdw.ac.id>
2022-05-17 19:15:49 +0700

    add susun huruf, pencapaian

---
+++ AplikasiBelajarAnak/app/src/main/java/com/example/aba/ui/latihan/SusunHurufActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aba.ui.latihan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aba.R
+
+class SusunHurufActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_susun_kata)
+    }
+}
---
+++ AplikasiBelajarAnak/app/src/main/java/com/example/aba/ui/pencapaian/PencapaianActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aba.ui.pencapaian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PencapaianActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pencapaian)
+    }
+}
