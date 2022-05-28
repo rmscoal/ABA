7fca51dfb53456f08ee59e4b232e76e520bccff8
rmscoal <rifky.m.s@mail.ugm.ac.id>
2022-05-28 12:59:38 +0700

    Update Code

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,12 +1,13 @@
 /* Initialize connecting to MySQL Database */
+require('dotenv').config({path: '/Users/rmscoal/GitHub/ABA/backend/utils/.env'});
 
-const mysql = require('mysql') 
+const mysql = require('mysql')
 
 const con = mysql.createConnection({
-    host: 'localhost', // <This  is development database>
-    user: 'root', // <This  is development database>
-    password: '', // <This  is development database>
-    database: 'aba' // <This  is development database>
+    host: process.env.DB_HOST, // <This  is development database>
+    user: process.env.DB_USER_NAME, // <This  is development database>
+    password: process.env.DB_DATABASE_PASSWORD, // <This is development database>is development database>
+    database: process.env.DB_DATABASE_NAME, // <This is development database>is development database>
 })
 
 module.exports = con;
