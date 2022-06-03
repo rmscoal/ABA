0be1ff9bc1e48380205b9734c773761c5b4ed071
rmscoal <rifky.m.s@mail.ugm.ac.id>
2022-06-03 11:29:44 +0700

    Add Startup Script

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,18 +1,19 @@
 # This is a file for automation startup scripts in VM
+# sudo apt install git
+# git clone https://github.com/rmscoal/ABA.git
 sudo apt-get update && sudo apt update
+sudo apt install g++ gcc make
 sudo apt install python3
 sudo apt install python3-pip 
-# sudo apt install git
+sudo pip install numpy
+sudo pip install librosa
+sudo pip install tensorflowjs
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 nvm install v16.15.0
-# git clone https://github.com/rmscoal/ABA.git
-sudo pip install librosa
-sudo pip install numpy
-sudo apt install g++ gcc make
-sudo pip install tensorflowjs
+
+npm install -g pm2
 cd ABA/backend && npm install
-cd && npm install -g pm2
