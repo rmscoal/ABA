7e62839aef1e4cbfe5e23c1bca3450ea16a311f8
rmscoal <rifky.m.s@mail.ugm.ac.id>
2022-06-05 14:48:02 +0700

    Add Predict Kata

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -7,7 +7,7 @@ const mysql = require('mysql')
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD_DEV,
     database: process.env.DB_DATABASE
