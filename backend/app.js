c239f9e5228030885bedf259e221155aba9b7a17
rmscoal <rifky.m.s@mail.ugm.ac.id>
2022-08-18 22:44:34 +0700

    Code Cleaning and Fixes

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,57 @@
-/* 
-  @ IMPORT MODULES 
-*/
-const express = require('express');
-const http = require('http');
-const path = require('path');
-require('dotenv').config({path: path.join(__dirname, 'config.env')});
+const express = require("express");
+const http = require("http");
+const path = require("path");
+require("dotenv").config({
+  path: path.join(__dirname, "config.env")
+});
 
-/* 
-  @ IMPORT ROUTES FROM OTHER DIRECTORIES 
-*/
-const userRoute = require('./routes/userRoute');
-const achievementRoute = require('./routes/achievementRoute');
-const predictionRoute = require('./routes/predictionRoute');
-const healthRoute = require('./routes/healthRoute');
-const rimakataRoute = require('./routes/rimakataRoute');
+// These are the options for response headers.
+const options = {
+  etag: false,
+  extensions: ["htm", "html"],
+  index: false,
+  maxAge: "1d",
+  redirect: false,
+  setHeaders: function (res, _path, _stat) {
+    res.set("x-timestamp", Date.now());
+  }
+};
 
-/* 
-  @ UTILS 
-*/
+/**
+ * @import available routes in the application
+ */
+const userRoute = require("./routes/userRoute");
+const achievementRoute = require("./routes/achievementRoute");
+const predictionRoute = require("./routes/predictionRoute");
+const healthRoute = require("./routes/healthRoute");
+const rimakataRoute = require("./routes/rimakataRoute");
+
 const port = process.env.PORT_HTTP;
-const hostname = require('./utils/host');
+const hostname = require("./utils/host");
 
-/* 
-  @ IMPORT MIDDLEWARE FOR FIREBASE AUTHORIZATION
-*/
-const middlewareFirebase = require('./middleware/firebaseAdmin.js');
+/**
+ * @middleware
+ */
+const middlewareFirebase = require("./middleware/firebaseAdmin.js");
 
-/* 
-  @ APP 
-*/
+/**
+ * @server
+ */
 const app = express();
-
 app.use(express.json());
-app.use(express.urlencoded({extended: false})); // Don't forget to set the header Content-Type: application/json
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public", options));
 
-/* 
-  @ ROUTES
-*/
-app.use('/users', middlewareFirebase.decodeToken, userRoute);
-app.use('/achievements', middlewareFirebase.decodeToken, achievementRoute);
-app.use('/predictions', middlewareFirebase.decodeToken, predictionRoute);
-app.use('/rimakatawords', middlewareFirebase.decodeToken, rimakataRoute);
-app.use('/healthchecks', healthRoute);
+/**
+ * @routes
+ */
+app.use("/users", middlewareFirebase.decodeToken, userRoute);
+app.use("/achievements", middlewareFirebase.decodeToken, achievementRoute);
+app.use("/predictions", middlewareFirebase.decodeToken, predictionRoute);
+app.use("/rimakatawords", middlewareFirebase.decodeToken, rimakataRoute);
+app.use("/healthchecks", healthRoute);
 
-/* 
-  @ START APP 
-*/
 const httpServer = http.createServer(app);
 httpServer.listen(port, () => {
   console.log(`App is listening in ${hostname} on ${port}!`);
-})
+});
--- backend/handlers/getUserDataHandler.js
+++ backend/handlers/getUserDataHandler.js
@@ -1,87 +1,114 @@
-/* Get all achievements from user */
+const winston = require("winston");
+const { LoggingWinston } = require("@google-cloud/logging-winston");
 
-/* 
-    @ IMPORT MODULES
-*/
-const winston = require('winston');
-const {LoggingWinston} = require('@google-cloud/logging-winston');
-
-// initiate logging winston
+/**
+ * @logging initiate winston and google cloud logging to log activity.
+ */
 const loggingWinston = new LoggingWinston();
-
 const logger = winston.createLogger({
-    level: 'info',
-    transports: [
-        new winston.transports.Console(),
-        loggingWinston,
-    ],
-})
+  level: "info",
+  transports: [
+    new winston.transports.Console(),
+    loggingWinston
+  ]
+});
 
-/* 
-  @ IMPORT MODULES FROM UTILS TO QUERY THE DATABASE
-*/ 
-const getSpecificUserData = require('../utils/getSpecificUserData'); // 
+/**
+ * @import module from the database queries folder.
+ */
+const getSpecificUserData = require("../utils/getSpecificUserData");
 
-// currently next() is not implemented
-const getUserDataHandler = async (req,res, _next) => {
-    // req.user does not exist 
-    if (!req.user) {
-        // return a bad request error
-        return res.status(401).json({
-            status: 'fail', 
-            type: 'user/user-unidentified',
-            message: 'req.user does not exist! It is required to query data.'
-        })
-    }
-    const { id } = req.user; // get the id of the user
-    // handles the promise 
-    getSpecificUserData(id)
-        // resultQuery returns an array 
-        .then((resultQuery) => {
-            // handles data not found
-            if (resultQuery.length < 1) {
-                // log to winston_log
-                logger.error('user/user-data-not-found');
-                return res.status(404).json({
-                    status: 'fail', 
-                    type: 'user/user-data-not-found',
-                    message: 'Empty result for user\'s query!'
-                })
-            }
-            const result = resultQuery[0]; // sets the result
-            const dataToSendResponse = {
-                "nama_user": result.nama_user,
-                "achv_id": result.achv_id,
-                "user_id": result.user_id,
-                "eksplor_huruf": Object.keys(JSON.parse(result.eksplor_huruf)),
-                "eksplor_angka": Object.keys(JSON.parse(result.eksplor_angka)),
-                "latMenyusunKatalvl1": result.latMenyusunKatalvl1,
-                "latMenyusunKatalvl2": result.latMenyusunKatalvl2,
-                "latMenyusunKatalvl3": result.latMenyusunKatalvl3,
-                "latMengejaHuruflvl1": Object.keys(JSON.parse(result.latMengejaHuruflvl1)),
-                "latMengejaHuruflvl2": Object.keys(JSON.parse(result.latMengejaHuruflvl2)),
-                "latMengejaHuruflvl3": Object.keys(JSON.parse(result.latMengejaHuruflvl3)),
-            }
-            // log to winston_log
-            logger.info('Request on user data for id: ' + id);
-            // return the result from the query
-            return res.status(200).json({
-                status: 'success', 
-                message: 'Query user data successfully done!',
-                data: dataToSendResponse
-            })
-        })
-        // error handling
-        .catch((err) => {
-            // log to winston_log
-            logger.error('database/fail-to-query');
-            // sends back response to user
-            return res.status(500).json({
-                status: 'fail', 
-                type: 'database/fail-to-query',
-                message: err.message
-            })
-        })
-}
+/**
+ * execFunction() is the executor function for getUserDataHandler() function
+ * to make code structure clean and organized. The task of execFunction() is
+ * to query data using the requested id and returns response beginning in int
+ * to identify whether it has succeeded or failed during the query process.
+ * @param {*} id of the user on requesting data.
+ * @returns a tuple of (code, result) where:
+ *
+ * 1 indicates that it has successfully retrieve user data from database,
+ * -1 indicates that user data is not found in the database, and
+ * -2 indicates that there is an error during database query.
+ */
+const execFunction = async (id) => {
+  // With the ID obtained, query user data from the database.
+  getSpecificUserData(id)
+    .then((resultQuery) => {
+      if (resultQuery.length < 1) {
+        logger.error("user/user-data-not-found");
+        // eslint-disable-next-line prefer-const
+        let err = "user-data-not-found";
+        return (-1, err);
+      };
+
+      const result = resultQuery[0];
+      const dataToSendResponse = {
+        nama_user: result.nama_user,
+        achv_id: result.achv_id,
+        user_id: result.user_id,
+        eksplor_huruf: Object.keys(JSON.parse(result.eksplor_huruf)),
+        eksplor_angka: Object.keys(JSON.parse(result.eksplor_angka)),
+        latMenyusunKatalvl1: result.latMenyusunKatalvl1,
+        latMenyusunKatalvl2: result.latMenyusunKatalvl2,
+        latMenyusunKatalvl3: result.latMenyusunKatalvl3,
+        latMengejaHuruflvl1: Object.keys(JSON.parse(result.latMengejaHuruflvl1)),
+        latMengejaHuruflvl2: Object.keys(JSON.parse(result.latMengejaHuruflvl2)),
+        latMengejaHuruflvl3: Object.keys(JSON.parse(result.latMengejaHuruflvl3))
+      };
+
+      logger.info("Requested user data on ID: " + id);
+      return (1, dataToSendResponse);
+    })
+    .catch((err) => {
+      logger.error("database/fail-to-query");
+      return (-2, err.message);
+    });
+};
+
+/**
+ * getUserDataHandler() handles specific user data query from the database.
+ * To run this function, the user request must firstly be authenticated via
+ * the middleware. With that, we can use the user.id to retrieve its data from
+ * the database. This function takes advantage of execFunction to the do job
+ * on querying from the database. Using this structure makes the code cleaner
+ * and managable.
+ * @param {*} req the request from client-side.
+ * @param {*} res the response it will give to the client.
+ * @param {*} _next currently next() is not implemented
+ * @returns res
+ */
+const getUserDataHandler = async (req, res, _next) => {
+  // If req.user does not exist returns a fail response.
+  if (!req.user) {
+    return res.status(401).json({
+      status: "fail",
+      type: "user/user-unidentified",
+      message: "req.user does not exist! It is required to query data."
+    });
+  };
+
+  const { id } = req.user;
+  const { code, result } = await execFunction(id);
+  switch (code) {
+    case 1:
+      return res.status(200).json({
+        status: "fail",
+        message: "QUERY USER DATA SUCCESFULLY DONE!",
+        data: result
+      });
+    case -1:
+      return res.status(404).json({
+        status: "fail",
+        type: "user/user-data-npt-found",
+        message: "EMPTY RESULT FOR USER'S QUERY!"
+      });
+    case -2:
+      return res.status(500).json({
+        status: "fail",
+        type: "database/fail-to-query",
+        message: result
+      });
+  }
+};
 
 module.exports = getUserDataHandler;
