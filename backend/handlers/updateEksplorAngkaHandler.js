6d57a94006a10ee51931201d15925816ac39d8ce
rmscoal <rifky.m.s@mail.ugm.ac.id>
2022-09-13 17:32:13 +0700

    Beautify Code

--- backend/handlers/updateEksplorAngkaHandler.js
+++ backend/handlers/updateEksplorAngkaHandler.js
@@ -1,86 +1,71 @@
-/* Handler to update/PUT achievements in eksplor-angka section */
-
-/* 
-    @ IMPORT MODULES
-*/
-const winston = require('winston');
-const {LoggingWinston} = require('@google-cloud/logging-winston');
+const winston = require("winston");
+const { LoggingWinston } = require("@google-cloud/logging-winston");
 
 // initiate logging winston
 const loggingWinston = new LoggingWinston();
 
 const logger = winston.createLogger({
-    level: 'info',
-    transports: [
-        new winston.transports.Console(),
-        loggingWinston,
-    ],
-})
+  level: "info",
+  transports: [
+    new winston.transports.Console(),
+    loggingWinston
+  ]
+});
 
-/* 
-  @ IMPORT MODULES FROM UTILS TO QUERY THE DATABASE
-*/ 
-const updateEksplorAngkaDatabaseQuery = require('../utils/updateEksplorAngkaDatabaseQuery');
+const updateEksplorAngkaDatabaseQuery = require("../utils/updateEksplorAngkaDatabaseQuery");
 
-// currently next() is not implemented
-// EA stands for eksplor angka
-const updateEA = async (req, res, next) => {
-    if (!req.user) {
-        // return a bad request error
-        return res.status(401).json({
-            status: 'fail', 
-            type: 'user/user-unidentified',
-            message: 'req.user does not exist! It is required to query data.'
-        })
-    }
-    
-    const { id } = req.user; // get id from the user
-    const { eksplorAngkaData } = req.body // get data from the request body as an object
-    // add handler if eksplorAngkaData is undefined, for example frontend sent the wrong format
-    /* 
-        example req.body
-        {
-            eksplorAngkaData: {
-                nol: true,
-                satu: true,
-                dua: true,
-                etc... 
-            }
-        }
-    */
-    updateEksplorAngkaDatabaseQuery(id, eksplorAngkaData)
-        // resultQuery returns an object 
-        .then((resultQuery) => {
-            // handles no rows being affected from the query
-            if (resultQuery.changedRows < 1 && resultQuery.affectedRows < 1) {
-                // log to winston_log
-                logger.error('database/no-affected-rows');
-                // sends back response to user
-                return res.status(400).json({
-                    status: 'fail',
-                    type: 'database/no-affected-rows',
-                    message: 'No rows are being affected on this query.'
-                })
-            }
-            // log to winston_log
-            logger.info('Request update score for id: ' + id);
-            // sends back response to user
-            return res.status(200).json({
-                status: 'success',
-                message: 'User\'s achievements on eksplor angka successfully updated!'
-            })
-        })
-        .catch((err) => {
-            // log to winston_log
-            logger.error('database/fail-to-query');
-            // sends back response to user
-            return res.status(500).json({
-                status: 'fail',
-                type: 'database/fail-to-query',
-                message: err.message
-            })
-        })
+/**
+ * This action is used to update eksplor angka in activity.
+ * @param {*} req request from client-side.
+ * @param {*} res response to client-side
+ * @param {*} next currently not implemented.
+ * @returns res.
+ */
+const updateEA = async (req, res, _next) => {
+  if (!req.user) {
+    // return a bad request error
+    return res.status(401).json({
+      status: "fail",
+      type: "user/user-unidentified",
+      message: "req.user does not exist! It is required to query data."
+    });
+  };
 
-}
+  const { id } = req.user; // get id from the user
+  const { eksplorAngkaData } = req.body; // get data from the request body as an object
 
-module.exports = updateEA; 
+  updateEksplorAngkaDatabaseQuery(id, eksplorAngkaData)
+  // resultQuery returns an object
+    .then((resultQuery) => {
+      // handles no rows being affected from the query
+      if (resultQuery.changedRows < 1 && resultQuery.affectedRows < 1) {
+        // log to winston_log
+        logger.error("database/no-affected-rows");
+        // sends back response to user
+        return res.status(400).json({
+          status: "fail",
+          type: "database/no-affected-rows",
+          message: "No rows are being affected on this query."
+        });
+      }
+      // log to winston_log
+      logger.info("Request update score for id: " + id);
+      // sends back response to user
+      return res.status(200).json({
+        status: "success",
+        message: "User's achievements on eksplor angka successfully updated!"
+      });
+    })
+    .catch((err) => {
+      // log to winston_log
+      logger.error("database/fail-to-query");
+      // sends back response to user
+      return res.status(500).json({
+        status: "fail",
+        type: "database/fail-to-query",
+        message: err.message
+      });
+    });
+};
+
+module.exports = updateEA;
--- backend/handlers/updateEksplorHurufHandler.js
+++ backend/handlers/updateEksplorHurufHandler.js
@@ -1,84 +1,71 @@
-/* Handler to update/PUT achievements in eksplor-huruf section */
-
-/* 
-    @ IMPORT MODULES
-*/
-const winston = require('winston');
-const {LoggingWinston} = require('@google-cloud/logging-winston');
+const winston = require("winston");
+const { LoggingWinston } = require("@google-cloud/logging-winston");
 
 // initiate logging winston
 const loggingWinston = new LoggingWinston();
 
 const logger = winston.createLogger({
-    level: 'info',
-    transports: [
-        new winston.transports.Console(),
-        loggingWinston,
-    ],
-})
+  level: "info",
+  transports: [
+    new winston.transports.Console(),
+    loggingWinston
+  ]
+});
 
-/* 
-  @ IMPORT MODULES FROM UTILS TO QUERY THE DATABASE
-*/ 
-const updateEksplorHurufDatabaseQuery = require('../utils/updateEksplorHurufDatabaseQuery');
+const updateEksplorHurufDatabaseQuery = require("../utils/updateEksplorHurufDatabaseQuery");
 
-// currently next() is not implemented
-// EH stands for eksplor huruf
-const updateEH = async (req, res, next) => {
-    if (!req.user) {
-        // return a bad request error
-        return res.status(401).json({
-            status: 'fail', 
-            type: 'user/user-unidentified',
-            message: 'req.user does not exist! It is required to query data.'
-        })
-    }
-    
-    const { id } = req.user; // get id from the user
-    const { eksplorHurufData } = req.body // get data from the request body as an object
-    /* 
-        example req.body
-        {
-            eksplorHurufData: {
-                a: true,
-                b: true,
-                etc... 
-            }
-        }
-    */
-    updateEksplorHurufDatabaseQuery(id, eksplorHurufData)
-        // resultQuery returns an object 
-        .then((resultQuery) => {
-            // handles no rows being affected from the query
-            if (resultQuery.changedRows < 1 && resultQuery.affectedRows < 1) {
-                // log to winston_log
-                logger.error('database/no-affected-rows');
-                // sends back response to user
-                return res.status(400).json({
-                    status: 'fail',
-                    type: 'database/no-affected-rows',
-                    message: 'No rows are being affected on this query.'
-                })
-            }
-            // log to winston_log
-            logger.info('Request update score for id: ' + id);
-            // sends back response to user
-            return res.status(200).json({
-                status: 'success',
-                message: 'User\'s achievements on eksplor huruf successfully updated!'
-            })
-        })
-        .catch((err) => {
-            // log to winston_log
-            logger.error('database/fail-to-query');
-            // sends back response to user
-            return res.status(500).json({
-                status: 'fail',
-                type: 'database/fail-to-query',
-                message: err.message
-            })
-        })
+/**
+ * This action is used to update eksplor huruf activity.
+ * @param {*} req request from client-side.
+ * @param {*} res response to clien-side.
+ * @param {*} _next currently not implemented.
+ * @returns res.
+ */
+const updateEH = async (req, res, _next) => {
+  if (!req.user) {
+    // return a bad request error
+    return res.status(401).json({
+      status: "fail",
+      type: "user/user-unidentified",
+      message: "req.user does not exist! It is required to query data."
+    });
+  };
 
-}
+  const { id } = req.user; // get id from the user
+  const { eksplorHurufData } = req.body; // get data from the request body as an object
 
-module.exports = updateEH; 
+  updateEksplorHurufDatabaseQuery(id, eksplorHurufData)
+  // resultQuery returns an object
+    .then((resultQuery) => {
+      // handles no rows being affected from the query
+      if (resultQuery.changedRows < 1 && resultQuery.affectedRows < 1) {
+        // log to winston_log
+        logger.error("database/no-affected-rows");
+        // sends back response to user
+        return res.status(400).json({
+          status: "fail",
+          type: "database/no-affected-rows",
+          message: "No rows are being affected on this query."
+        });
+      }
+      // log to winston_log
+      logger.info("Request update score for id: " + id);
+      // sends back response to user
+      return res.status(200).json({
+        status: "success",
+        message: "User's achievements on eksplor huruf successfully updated!"
+      });
+    })
+    .catch((err) => {
+      // log to winston_log
+      logger.error("database/fail-to-query");
+      // sends back response to user
+      return res.status(500).json({
+        status: "fail",
+        type: "database/fail-to-query",
+        message: err.message
+      });
+    });
+};
+
+module.exports = updateEH;
